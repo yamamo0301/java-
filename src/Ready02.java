d9ba9ba0f74b7c37d4b470de733681ca83821866
yamamoto <yamamo0301@icloud.com>
2022-10-27 10:29:20 +0900

    Chapter03 Finish

--- src/Ready02.java
+++ src/Ready02.java
@@ -1,6 +1,6 @@
 
 public class Ready02 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
