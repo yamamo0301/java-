7a66595865b0b66b844987e122ba0cfd097d4247
yamamoto <yamamo0301@icloud.com>
2022-10-29 14:33:10 +0900

    Chapter5 Finish

---
+++ src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234L;
+		double double1 = 1.234d;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 1.234f;
+		Integer int1 = 1234;
+		Character char1 = 'あ';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(char1);
+		
+		int[] num1 = new int[5];
+		num1[3] = 10; 
+		System.out.println(num1[3]);
+	}
+}
