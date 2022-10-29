66ce379e6bb3f3237fd1ffc382283abb8e3fd76c
yamamoto <yamamo0301@icloud.com>
2022-10-29 13:15:00 +0900

    Chapter04 Finish

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		final int NUM;
+		name = "山本裕也";
+		NUM = 3;
+		System.out.println(name);
+		
+		name = "日進月歩";
+		System.out.println(name);
+		
+		System.out.println(NUM);
+	}
+}
